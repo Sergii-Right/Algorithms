3d3d90e10645fb10131d10954b393d9a9618ad55
Sergii-Right <sergii.ziborov@gmail.com>
2017-01-20 23:19:00 +0200

    add iter to bubble sort and add checker

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -11,16 +11,27 @@ int main()
 
     //start Bubble Sort
     int size_array = sizeof(arr)/sizeof(arr[0]);
+    int a;
+    bool check = true;
+    int iter = 0;
     for(int i = 0; i < size_array; ++i){
         for(int j = 1; j < size_array - i; ++j){
             if(arr[j-1] > arr[j]){
-                arr[j-1] ^= (arr[j] ^= arr[j-1]);
-                arr[j] ^= arr[j-1];
+                a = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = a;
+                check = false;
             }
+            iter++;
         }
+        if(check){
+            break;
+        }
+        check = true;
     }
     //stop
-
+    printf("iter: %d\n",iter);
+    printf("\n---------\n");
     for(int i : arr){
         printf("%d, ", i);
     }
